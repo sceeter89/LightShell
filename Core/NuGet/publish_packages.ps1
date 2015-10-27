bce9035301062e5006ec6d21a80bf8f255971d12
Adam Marszalek <adam.marszalek@gmail.com>
2015-10-27 23:52:18 +0100

    Added NuGet packages definitions with script for automated publishing.

---
+++ Core/NuGet/publish_packages.ps1
@@ -0,0 +1,23 @@
+ param (
+    [Parameter(Mandatory=$true)][string]$version
+ )
+
+$cache_dir = "publish_cache"
+
+if( (Test-Path -Path $cache_dir)) {
+	Remove-Item -Recurse -Force $cache_dir
+}
+
+New-Item $cache_dir -type directory
+
+$nuspecs = Get-Childitem "." -Recurse | where {$_.extension -eq ".nuspec"}
+foreach ($spec_path in $nuspecs) {
+	nuget pack -OutputDirectory $cache_dir -Version $version $spec_path.FullName
+}
+
+$packages = Get-Childitem $cache_dir | where {$_.extension -eq ".nupkg"}
+foreach ($package in $packages) {
+	nuget push $package.FullName
+}
+
+Write-Host "Publish completed!"
